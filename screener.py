e2616b4f4c6bcf21a8e8f5bb8fe44f41940bd254
Nikhil Kini <nkini@ucsc.edu>
2016-06-01 14:52:04 -0700

    Non failing screener

--- screener.py
+++ screener.py
@@ -32,13 +32,13 @@ def screen(inp, token_map=token_map):
         token_type = token.type
         token_value = token.value
 
-        if token_class == 'WS': continue
-        elif token_class == 'LPAREN' or 'RPAREN': output.append(token)
-        elif token_class == 'NUM': output.append(token)
+        if token_type == 'WS': continue
+        elif token_type == 'LPAREN' or 'RPAREN': output.append(token)
+        elif token_type == 'NUM': output.append(token)
 
-        elif token_class in token_map:
-            if token_value in token_map[token_class]:
-                output.append(token_map[token_class][token_value])
+        elif token_type in token_map:
+            if token_value in token_map[token_type]:
+                output.append(token_map[token_type][token_value])
             else:
                 output.append(Token('VAR',token_value))
         else: "Print something's not right"
