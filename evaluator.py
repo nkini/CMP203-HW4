ab82c0214c6d6b89987211b25dfe28fe5e0ade4f
Nikhil Kini <nkini@ucsc.edu>
2016-06-03 04:57:09 -0700

    Works till question 10 still, but some improvement made

--- evaluator.py
+++ evaluator.py
@@ -13,11 +13,31 @@ def step(C, E, K):
 
     global outstring
 
-    #print(type(C))
+    print("\n")
+    print("type(C)",type(C))
+    print("C is ",C)
     #print("C,E,K:")
     #print('\t'.join(map(str,[C,E,K])))
+    if K: print(K[-1])
+
+    #CEK 2b
+    if type(C) == tuple and C[0].type == 'OP2':
+        #print('[cek2b]')
+        outstring += '  [cek2b]\n'
+        M = C[1]
+        N = C[2]
+        K.append(Token('ARG12', (C[0].value,(N,E))))
+        return (M),E
+
+
+    #CEK 4
+    if K and isinstance(K[-1],Token) and K[-1].type == 'ARG':
+        retval = cek4(C,E,K)
+        if retval: return retval
 
     if type(C) == tuple:
+        
+
         #CEK 1
         #if isinstance(C[0], Token) and C[0].type == 'APP'
         if C[0].type == 'APP':
@@ -28,14 +48,6 @@ def step(C, E, K):
             K.append(Token('ARG',(N,E)))
             return (M), E
 
-        #CEK 2b
-        if C[0].type == 'OP2':
-            #print('[cek2b]')
-            outstring += '  [cek2b]\n'
-            M = C[1]
-            N = C[2]
-            K.append(Token('ARG12', (C[0].value,(N,E))))
-            return (M),E
 
         #CEK 2a
         if C[0].type == 'OP1':
@@ -52,47 +64,38 @@ def step(C, E, K):
         if C[0].type == 'LAM' and not K:
             return C,E
 
-        #CEK 4
-        if C[0].type == 'LAM' and K[-1].type == 'ARG':
-            #print('[cek4]')
-            outstring += '  [cek4]\n'
-            V = C
-            e = E
-            k = K.pop()
-            N = k.value[0]
-            e_prime = k.value[1]
-            K.append(Token('FUN',(V,e)))
-            return (N), e_prime
+        '''
+        if C[0].type == 'LAM':
+            print("This is K[-1]",K[-1])
+            if K[-1].type == 'ARG':
+                print('[cek4]')
+                outstring += '  [cek4]\n'
+                V = C
+                e = E
+                k = K.pop()
+                N = k.value[0]
+                e_prime = k.value[1]
+                K.append(Token('FUN',(V,e)))
+                return (N), e_prime
+            #retval = cek4(C[0],E,K)
+            #if retval: return retval
+        '''
             
-
+        #CEK 3
+        if K and K[-1].type == 'FUN':
+            return cek3(C[0],E,K)
  
     if isinstance(C, Token):
-
-        #CEK 7  
-        #if the control is a variable, we look it up in the environment
-        if C.type == 'VAR':
-            if E:
-                '''
-                    C,E,K:
-                    Token(type='VAR', value='x')    [(Token(type='VAR', value='x'), 3, [...])]  []
-                '''
-                #print('[cek7]')
-                outstring += '  [cek7]\n'
-                c = lookup(E,C)
-                #print(c)
-                return c
-
-            #if environment is empty, return an error
-            else:
-                return None,None
-
-        if not K:
-            return C, E
 
 
         #print(K[-1])
         #CEK 3
-        if C.type == 'NUM' and K[-1].type == 'FUN':
+        if K and K[-1].type == 'FUN':#C.type == 'NUM' 
+            #print("got here")
+            retval = cek3(C,E,K)
+            if retval: return retval
+            '''
+            #Delete the rest of this if the function call works
             if (type(K[-1].value[0]) == tuple and K[-1].value[0][0].type == 'LAM') or \
                (type(K[-1].value[0]) == Token and K[-1].value[0].type == 'LAM'):
 
@@ -110,9 +113,37 @@ def step(C, E, K):
                 #exit()
                 e_prime.append((X,(V,E)))
                 return (M),e_prime
+            '''
+        '''
+        if C.type == 'LAM':
+            retval = cek4(C,E,K)
+            if retval: return retval
+        '''
+
+        #CEK 7  
+        #if the control is a variable, we look it up in the environment
+        if C.type == 'VAR':
+            if E:
+                '''
+                    C,E,K:
+                    Token(type='VAR', value='x')    [(Token(type='VAR', value='x'), 3, [...])]  []
+                '''
+                #print('[cek7]')
+                outstring += '  [cek7]\n'
+                c = lookup(E,C)
+                print("This is the c in cek7:",c)
+                return c
+
+            #if environment is empty, return an error
+            else:
+                return None,None
+
+        if not K:
+            return C, E
 
         #CEK 4
         #if isinstance(C[0], Token) and C[0].type == 'NUM' and K[-1].type == 'ARG':
+        '''
         if C.type == 'NUM' and K[-1].type == 'ARG':
             #print('[cek4]')
             outstring += '  [cek4]\n'
@@ -123,7 +154,7 @@ def step(C, E, K):
             e_prime = k.value[1]
             K.append(Token('FUN',(V,e)))
             return (N), e_prime
-
+        '''
 
         #CEK 5a
         if C.type == 'NUM' and K[-1].type == 'ARG11':
@@ -161,6 +192,53 @@ def step(C, E, K):
     return None, None
 
 
+def cek4(C,E,K):
+
+    global outstring
+
+    print("This is K[-1]",K[-1])
+    if K[-1].type == 'ARG':
+        print('[cek4]')
+        outstring += '  [cek4]\n'
+        V = C
+        e = E
+        k = K.pop()
+        N = k.value[0]
+        e_prime = k.value[1]
+        K.append(Token('FUN',(V,e)))
+        return (N), e_prime
+
+def cek3(C,E,K):
+    global outstring
+    if K and K[-1].type == 'FUN':#C.type == 'NUM' 
+        print("got here")
+        if (type(K[-1].value[0]) == tuple and K[-1].value[0][0].type == 'LAM') or \
+           (type(K[-1].value[0]) == Token and K[-1].value[0].type == 'LAM'):
+            
+            print(['cek3'])
+            print("K[-1].value[0] is:",K[-1].value[0])
+            outstring += '  [cek3]\n'
+            k = K.pop()
+            if type(k.value[0]) == tuple:
+                X = k.value[0][1][0]
+                M = k.value[0][1][1]
+                e_prime = k.value[1]
+            else:
+                print("k is: ",k)
+                X = k.value[1]
+                M = k.value[1]
+                e_prime = k.value[1]
+            V = C#C.value
+            print("e_prime:",e_prime)
+            print("X:",X)
+            print("V:",V)
+            print("E:",E)
+            #exit()
+            e_prime.append((X,(V,E)))
+            return (M),e_prime
+    
+
+
 def lookup(E, X):
     '''
         C,E,K:
@@ -196,7 +274,7 @@ def evaluate(ast):
     while True:
         control, environment = step(control, environment, stack)
 
-        #print("control, environment:",control, environment)
+        print("control, environment:",control, environment)
         if control is None and environment is None:
             return "Stuck"
 
@@ -324,6 +402,7 @@ if __name__ == '__main__':
         ast = parser.parse(screenout)
         #pprint(ast)
         retval = evaluate(ast)
+        print('\n')
         print("Evaluator returned:",retval)
         print(outstring)
         assert(outputs[i] == (outstring,retval))
